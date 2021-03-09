6105670935d0961b5420fe8e9c18bb90b689e01c
user_levin <levin.staudte@gmail.com>
2021-03-09 15:51:45 +0100

    add all files

---
+++ README.md
@@ -0,0 +1,3 @@
+This app was developed as part of a technical paper.
+
+Compression is ignored in the calculation!
---
+++ script.js
@@ -0,0 +1,13 @@
+function calc() {
+    let width = parseInt(document.getElementById("width").value);
+    let height = parseInt(document.getElementById("height").value);
+    let frame = parseInt(document.getElementById("frame").value);
+    let datadepth = parseInt(document.getElementById("datadepth").value);
+    let time = parseInt(document.getElementById("time").value);
+  
+    let result = (width * height * frame * datadepth * time) / (8 * Math.pow(1024, 2));
+    
+    
+    document.getElementById("result").textContent = result.toPrecision(4);
+    
+  }
