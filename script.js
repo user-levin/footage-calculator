d0389f2e68587c7b00b495a6f69e468f90255d82
user_levin <levin.staudte@gmail.com>
2021-03-10 17:54:48 +0100

    git test

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function calc() {
     let time = parseInt(document.getElementById("time").value);
   
     let result = (width * height * frame * datadepth * time) / (8 * Math.pow(1024, 2));
-    
-    
+
     document.getElementById("result").textContent = result.toPrecision(4);
-    
   }
+
+  //git test
