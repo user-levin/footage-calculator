9352317b7d095f7ad1550bd5480f50dfd2b8ef20
user-levin <72907873+user-levin@users.noreply.github.com>
2021-03-12 01:43:49 +0100

    jumb at the menu

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //calc
 function calc() {
-    let width = parseInt(document.getElementById("width").value);
-    let height = parseInt(document.getElementById("height").value);
-    let frame = parseInt(document.getElementById("frame").value);
-    let datadepth = parseInt(document.getElementById("datadepth").value);
-    let time = parseInt(document.getElementById("time").value);
-  
-    let result = (width * height * frame * datadepth * time) / (8 * Math.pow(1024, 2));
+  let width = parseInt(document.getElementById("width").value);
+  let height = parseInt(document.getElementById("height").value);
+  let frame = parseInt(document.getElementById("frame").value);
+  let datadepth = parseInt(document.getElementById("datadepth").value);
+  let time = parseInt(document.getElementById("time").value);
 
-    document.getElementById("result").textContent = result.toPrecision(4);
-  }
+  let result = (width * height * frame * datadepth * time) / (8 * Math.pow(1024, 2));
+
+  document.getElementById("result").textContent = result.toPrecision(4);
+}
 
 
-  function goingtothedarkside() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+function goingtothedarkside() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
 
 //for copyright
