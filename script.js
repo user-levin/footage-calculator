790318fccdee80c8433a1b8805766bc2679d9b81
user-levin <72907873+user-levin@users.noreply.github.com>
2021-03-13 01:38:26 +0100

    css; menu bug fix; calc storage and time

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 //calc
-function calc() {
-  let width = parseInt(document.getElementById("width").value);
-  let height = parseInt(document.getElementById("height").value);
-  let frame = parseInt(document.getElementById("frame").value);
-  let datadepth = parseInt(document.getElementById("datadepth").value);
-  let time = parseInt(document.getElementById("time").value);
+function calcspace() {
+  let widthspace = parseInt(document.getElementById("widthspace").value);
+  let heightspace = parseInt(document.getElementById("heightspace").value);
+  let framespace = parseInt(document.getElementById("framespace").value);
+  let datadepthspace = parseInt(document.getElementById("datadepthspace").value);
+  let timespace = parseInt(document.getElementById("timespace").value);
 
-  let result = (width * height * frame * datadepth * time) / (8 * Math.pow(1024, 2));
+  let resultspace = (widthspace * heightspace * framespace * datadepthspace * timespace) / (8 * Math.pow(1024, 2));
 
-  document.getElementById("result").textContent = result.toPrecision(4);
+  document.getElementById("resultspace").textContent = Math.round(resultspace);
 }
 
+function calctime() {
+  let widthtime = parseInt(document.getElementById("widthtime").value);
+  let heighttime = parseInt(document.getElementById("heighttime").value);
+  let frametime = parseInt(document.getElementById("frametime").value);
+  let datadepthtime = parseInt(document.getElementById("datadepthtime").value);
+  let spacetime = parseInt(document.getElementById("spacetime").value);
+
+  let resulttime = (8 * Math.pow(1024, 2) * spacetime) / (widthtime * heighttime * datadepthtime * frametime);
+
+  document.getElementById("resulttime").textContent = Math.round(resulttime);
+}
 
 function goingtothedarkside() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
 
-
 //for copyright
-var today = new Date();
-var year = today.getFullYear();
-var copyright = "&copy;" + " Levin Staudte" + " " + year; 
+  var today = new Date();
+  var year = today.getFullYear();
+  var copyright = "&copy;" + " Levin Staudte" + " " + year; 
