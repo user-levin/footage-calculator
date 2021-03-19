b374cb0f7ff4db896f34bdf74a39be7d60d38d58
user-levin <72907873+user-levin@users.noreply.github.com>
2021-03-20 00:46:00 +0100

    localStorage for Darke Mode; add Schnellauswahlformat

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+//Dark Mode
+function getBool(val) {
+  return !!JSON.parse(String(val).toLowerCase());
+}
+
+function checkdarkmodestate() {
+
+  let isdark = getBool(localStorage.getItem("darkmode"));
+
+  if (isdark) {
+    setDarkMode();
+  }
+}
+
+
+
+const modename = "dark-mode";
+
+function setDarkMode() {
+  const completesite = document.body;
+
+  if (completesite.classList.contains(modename)) {
+    return;
+  }
+  completesite.classList.add(modename);
+  localStorage.setItem("darkmode", true);
+}
+
+function setBrightMode() {
+  const completesite = document.body;
+  if (completesite.classList.contains(modename)) {
+    completesite.classList.remove("dark-mode")
+  }
+  localStorage.setItem("darkmode", false);
+}
+
+
+function goingtothedarkside() {
+  const completesite = document.body;
+  if (completesite.classList.contains(modename)) {
+    setBrightMode()
+  } else {
+    setDarkMode()
+  }
+}
+
+
+
 //calc space for Movie
 function calcspace() {
   let widthspace = parseInt(document.getElementById("widthspace").value);
@@ -6,11 +54,33 @@ function calcspace() {
   let datadepthspace = parseInt(document.getElementById("datadepthspace").value);
   let timespace = parseInt(document.getElementById("timespace").value);
 
-  let resultspace = (widthspace * heightspace * framespace * datadepthspace * timespace) / (8 * Math.pow(1024, 2));
-  let resultspacegb = (resultspace / 1000);
+  let resultspace = calcspace2(widthspace, heightspace, framespace, datadepthspace, timespace);
+  //let resultspacegb = (resultspace / 1000);
 
   document.getElementById("resultspace").textContent = Math.round(resultspace);
-  document.getElementById("resultspacegb").textContent = Math.round(resultspacegb);
+  //document.getElementById("resultspacegb").textContent = Math.round(resultspacegb);
+}
+
+function calcspace2(widthspace, heightspace, framespace, datadepthspace, timespace) {
+//function calcspace2(widthspace, heightspace, framespace, datadepthspace, timespace) {
+  return (widthspace * heightspace * framespace * datadepthspace * timespace) / (8 * Math.pow(1024, 2));
+}
+
+
+
+/**
+ *https://www.w3schools.com/js/js_const.asp
+ */
+//
+const sizes =
+{
+  "575": { "h": 1024, "w": 576 },
+  "720": { "h": 720, "w": 1280 },
+  "1080": { "h": 1080, "w": 1920 },
+  "4K": { "h": 3840, "w": 2160 },
+  "5K": { "h": 5120, "w": 2880 },
+  "6K": { "h": 5760, "w": 3240 },
+  "8K": { "h": 7690, "w": 4320 }
 }
 
 
@@ -44,19 +114,36 @@ function calcpic() {
 
 //var resultpic= parseInt(resultpic) + parseInt(mb);
 
+function renderSelection() {
+
+  let e = document.getElementById("schnellauswahlformat");
+  let width = sizes[e.options[e.selectedIndex].value].w;
+  let height = sizes[e.options[e.selectedIndex].value].h;
+  document.getElementById("selected").textContent = "Hoch: " + height + " | Breit: " + width;
+
+  // let widthspace = parseInt(document.getElementById("widthspace").value);
+  // let heightspace = parseInt(document.getElementById("heightspace").value);
+  // let framespace = parseInt(document.getElementById("framespace").value);
+  // let datadepthspace = parseInt(
+  //   document.getElementById("datadepthspace").value
+  // );
+  // let timespace = parseInt(document.getElementById("timespace").value);
+
+  // let resultspace = calcspace2(widthspace, heightspace, framespace, datadepthspace, timespace);
+  // let resultspacegb = (resultspace / 1000);
+
+  // document.getElementById("resultspace").textContent = Math.round(resultspace);
+  // document.getElementById("resultspacegb").textContent = Math.round(resultspacegb);
+}
+
 
 //for si units
 var gb = "in gb";
 var mb = "in mb";
 
 
-//Dark Mode
-function goingtothedarkside() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
 
-  
+
 //for copyright
 var today = new Date();
 var year = today.getFullYear();
